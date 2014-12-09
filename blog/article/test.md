7fc1e2098635ff0c7fca7fe5981ab92d50a01c5a
Salmon <grass.house@hotmail.com>
2014-12-10 00:02:09 +0800

    Add reset CSS.

---
+++ blog/article/test.md
@@ -0,0 +1,13 @@
+---
+title: 測試文章
+date:  2014/12/09
+time:  23:27
+---
+
+# Title
+
+## Subtitle
+
+1. item one
+2. item two
+3. item three
---
+++ blog/plugins/salmon.js
@@ -0,0 +1,20 @@
+function getArticleByTitle (title) {
+	$.ajax({
+		mimeType: 'text/plain; charset=utf-8',
+		url: 'article/' + title + '.md',
+		type: 'get',
+		dataType: 'text',
+		async: false,
+		success: function (data) {
+			var markdownToHTML = markdown.toHTML(data);
+			$('.content').html(markdownToHTML);
+		}
+	});
+}
+
+function getParameterByName (name) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+	results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
